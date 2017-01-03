35ca3c676a64f5b9446299eba3b00796bd0d8ac8
zhoudazhuang <yan.chou.strong@gmail.com>
2017-01-03 20:33:48 +0800

    start liongo

---
+++ main/liongo.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const VERSION = "0.0.1"
+
+const (
+	USAGE = `
+liongo is a static site generator in Go
+
+Usage:
+
+        liongo command [args...]
+
+The commands are:
+
+	build	        			build and generate site.
+	version         			print liongo version
+
+`
+)
+
+var httpAddr = ":8080"
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	argsLength := len(args)
+	fmt.Println(argsLength)
+	//判断输入命令长度
+	if argsLength == 0 || argsLength > 3 {
+		Usage()
+		os.Exit(1)
+	}
+	//通过第一个参数进行识别
+	switch args[0] {
+	default:
+		Usage()
+		os.Exit(1)
+	case "build":
+		//gosk.Build()
+	case "run":
+		if argsLength == 2 {
+			httpAddr = args[1]
+		}
+		fmt.Println("Listen at ", httpAddr)
+		//gosk.Run(httpAddr)
+	case "version":
+		fmt.Print("liongo version " + VERSION)
+	}
+}
+
+func Usage() {
+	fmt.Println(USAGE)
+}
