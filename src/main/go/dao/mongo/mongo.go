e96b9fde5664c2bca8ad48db8d26e6a78696a099
anteoy <anteoy@gmail.com>
2017-01-21 10:43:07 +0800

    mgo keep pool alive

--- src/main/go/dao/mongo/mongo.go
+++ src/main/go/dao/mongo/mongo.go
@@ -12,8 +12,24 @@ type Person struct {
 	Phone string
 }
 
+// 数据连接
+var Session *mgo.Session
+
+func init(){
+	// [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]
+	// mongodb://myuser:mypass@localhost:40001,otherhost:40001/mydb
+	var err error
+	Session, err = mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	// Optional. Switch the session to a monotonic behavior.
+	Session.SetMode(mgo.Monotonic, true)
+}
+
 func Mgo() {
-	session, err := mgo.Dial("localhost:27017")
+	session, err := mgo.Dial("mongodb://localhost:27017")
 	if err != nil {
 		panic(err)
 	}
@@ -22,18 +38,4 @@ func Mgo() {
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
-	c := session.DB("test").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
 }
--- src/main/go/dao/mongo/mongo_test.go
+++ src/main/go/dao/mongo/mongo_test.go
@@ -1,7 +1,39 @@
 package mongo
 
-import "testing"
+import (
+	"testing"
+	"gopkg.in/mgo.v2/bson"
+	"fmt"
+	"log"
+)
 
 func TestMongo(t *testing.T) {
-	Mgo()
+	c := Session.DB("test").C("people")
+	err := c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
+	defer Session.Close()
+	cc := Session.DB("test2").C("people")
+	err = cc.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result2 := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
