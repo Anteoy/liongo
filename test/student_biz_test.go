5fd356f0f7ab4f8218e5533152ec498f3416461e
Anteoy <anteoy@gmail.com>
2017-04-12 21:51:37 +0800

    _test

---
+++ test/student_biz_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestGenerateToken(t *testing.T) {
+	//result,err := GenerateToken("1")
+	//t.Log(result)
+	//if err !=nil {
+	//	t.Error(err)
+	//	t.Error(result)
+	//}
+	//t.Log("sucess result = " + result.(string))
+}
