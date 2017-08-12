143a0ebc49fd41bbac4bece6b97bab02a8d8b09d
Anteoy <anteoy@gmail.com>
2017-08-12 23:51:21 +0800

    connected lionreact

--- controller/pNoteController.go
+++ controller/pNoteController.go
@@ -37,6 +37,9 @@ func init() {
 
 func (pNoteController *PNoteController) Login(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+	w.Header().Set("content-type", "application/json")             //返回数据格式是json
 	//start session
 	sess := globalSessions.SessionStart(w, r)
 	logrus.Debugf("Login() sessionID 为： %s", sess.SessionID())
