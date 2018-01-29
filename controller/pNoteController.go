1d7639a577170b5a408f5a1d7d6cd9464c5d003f
anteoy <anteoy@gmail.com>
2018-01-29 10:36:21 +0800

    must Access-Control-Allow-Headers Content-Type no *

--- controller/pNoteController.go
+++ controller/pNoteController.go
@@ -93,7 +93,7 @@ type LoginRreq struct {
 func (pNoteController *PNoteController) LoginR(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
-	w.Header().Add("Access-Control-Allow-Headers", "*") //header的类型
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 	w.Header().Set("content-type", "application/json")             //返回数据格式是json
 	//start session
 	sess := globalSessions.SessionStart(w, r)
