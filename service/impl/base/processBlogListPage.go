fc1964269b7ec21e9d110a0a8150e39fb1e62cbd
Anteoy <anteoy@gmail.com>
2018-02-16 20:08:12 +0800

    update count + 1

--- service/impl/base/processBlogListPage.go
+++ service/impl/base/processBlogListPage.go
@@ -53,15 +53,16 @@ func (processBlogList *ProcessBlogListPage) Dispose(dir string) {
 	pageSize := 10
 	//当前需要渲染的articlesl
 	for i := 0;i < totalPage;i++ {
-		targetFile := PUBLISH_DIR + "/blog_"+ strconv.Itoa(i) + ".html"
+		targetFile := PUBLISH_DIR + "/blog_"+ strconv.Itoa(i+1) + ".html"
 		var pre string
 		var next string
+		//update count + 1
 		if os.Getenv("liongo_env") == "online"{
-			pre = "http://anteoy.site" + "/blog_"+ strconv.Itoa(i-1) + ".html"
-			next = 	"http://anteoy.site" + "/blog_"+ strconv.Itoa(i+1) + ".html"
+			pre = "http://anteoy.site" + "/blog_"+ strconv.Itoa(i-1+1) + ".html"
+			next = 	"http://anteoy.site" + "/blog_"+ strconv.Itoa(i+1+1) + ".html"
 		}else {
-			pre = "http://127.0.0.1:8080" + "/blog_"+ strconv.Itoa(i-1) + ".html"
-			next = 	"http://127.0.0.1:8080" + "/blog_"+ strconv.Itoa(i+1) + ".html"
+			pre = "http://127.0.0.1:8080" + "/blog_"+ strconv.Itoa(i-1+1) + ".html"
+			next = 	"http://127.0.0.1:8080" + "/blog_"+ strconv.Itoa(i+1+1) + ".html"
 		}
 		fout, err := os.Create(targetFile)
 		if err != nil {
